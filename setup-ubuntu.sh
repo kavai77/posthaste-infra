69568805c448f0229035f0a6e742286c1d299b51
Csaba Kavai <csaba.kavai.osv@fedex.com>
2025-09-14 14:00:08 +0200

    changing k8s engine to microk8s

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,29 +1,16 @@
-# Install minikube: https://minikube.sigs.k8s.io/docs/start/?arch=%2Flinux%2Farm64%2Fstable%2Fbinary+download
-curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-arm64
-sudo install minikube-linux-arm64 /usr/local/bin/minikube && rm minikube-linux-arm64
-
-# Install Docker: https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
 sudo apt-get update
-sudo apt-get -y install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install snapd
 
-# add user to docker group
-sudo usermod -aG docker $USER && newgrp docker
+# Install MicroK8s
+sudo snap install microk8s --classic --channel=1.33
+sudo usermod -a -G microk8s $USER
+mkdir -p ~/.kube
+chmod 0700 ~/.kube
 
-# configure minikube, and kubectl
-minikube config set cpus 8
-minikube config set memory 14g
-minikube config set driver docker
-minikube start --static-ip 192.168.200.200
-minikube kubectl -- get po -A
+echo alias kubectl=\"microk8s kubectl\" >> .bash_aliases
+source .bash_aliases
+
+kubectl apply -f posthaste-infra/services.yaml
 
 # install and start nginx
 sudo apt-get -y install nginx
@@ -32,8 +19,6 @@ sudo systemctl enable nginx
 
 sudo apt-get -y install jq
 
-echo alias kubectl=\"minikube kubectl --\" >> .bash_aliases
-source .bash_aliases
 kubectl apply -f posthaste-infra/services.yaml
 
 sudo rm -rf /var/www/html
